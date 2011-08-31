545eebea79e34a712e86aac3d94998d236614f39
rwilliams <rwilliams@10.0.0.66>
2011-09-01 08:25:32 +1000

    Template the show page using mustache.

--- training/app/shows/album.js
+++ training/app/shows/album.js
@@ -1,14 +1,10 @@
 function(doc, req) {
-  var html = "<div data-role=\"page\" id=\"albumview\">" +
-                       "<div data-role=\"header\" id=\"albumheader\">" +
-                           "<h2 class=\"albumtitle\">" + doc.title + "<\/h2>" +
-                       "<\/div>" +
-                       "<div data-role=\"content\" id=\"albumcontent\">" +
-                           "<h2 class=\"artist\">" + doc.artist + "<\/h2>" +
-                           "<p class=\"title\">" + doc.title + "<\/p>" +
-                           "<p class=\"description\">" + doc.description + "<\/p>" +
-                       "<\/div>" +
-                       "<div data-role=\"footer\" \/>" +
-                   "<\/div>";
-  return html;
+    var Mustache = require("vendor/couchapp/lib/mustache");
+    var stash = {
+        artist: doc.artist,
+        title : doc.title,
+        description: doc.description,
+        document: doc._id
+    };
+    return Mustache.to_html(this.templates.album, stash);
 }
